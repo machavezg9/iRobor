05d3094f1974f9775ee3bd5ac262aa17314a56e4
Miguel <machavezg@icloud.com>
2016-08-17 16:21:08 -0700

    Update Piduino.py

--- iRobor/i2c-Pi-Arduino-Comm/Piduino.py
+++ iRobor/i2c-Pi-Arduino-Comm/Piduino.py
@@ -7,20 +7,20 @@ bus = smbus.SMBus(1)
 address = 0x04
 
 def writeNumber(value):
-bus.write_byte(address, value)
-# bus.write_byte_data(address, 0, value)
-return -1
+  bus.write_byte(address, value)
+  # bus.write_byte_data(address, 0, value)
+  return -1
 
 def readNumber():
-number = bus.read_byte(address)
-# number = bus.read_byte_data(address, 1)
-return number
+  number = bus.read_byte(address)
+  # number = bus.read_byte_data(address, 1)
+  return number
 
 while True:
-var = input("Enter 1 – 9: ")
-if not var:
-continue
-
+  var = input("Enter 1 – 9: ")
+  if not var:
+  continue
+  
 writeNumber(var)
 print "RPI: Hi Arduino, I sent you ", var
 # sleep one second
